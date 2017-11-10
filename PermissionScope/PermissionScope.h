bcd54090c3d08d5013b69ba44bfa0911b930b436
Lee Williams <lee.williams@proact.net>
2017-11-10 10:59:14 +0000

    updating to swift 4, please test before merge

--- PermissionScope/PermissionScope.h
+++ PermissionScope/PermissionScope.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double PermissionScopeVersionNumber;
 FOUNDATION_EXPORT const unsigned char PermissionScopeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <modalperms/PublicHeader.h>
-
-
